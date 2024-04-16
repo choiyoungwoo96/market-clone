b004300bf480e2b6c93082c4662b84c2d0395a27
choiyoungwoo96 <mariakjs96@naver.com>
2024-04-16 20:44:52 +0900

    회원 가입 로직 기능 구현
    프론트엔드 및 백엔드 작업 signup

--- main.py
+++ main.py
@@ -20,11 +20,12 @@ cur.execute(f"""
                 description TEXT,
                 place TEXT NOT NULL
             );
+        """)
 app = FastAPI()
 
 
 
-            """)
+            
 @app.post("/items")
 async def create_item(image:UploadFile,
                 title:Annotated[str,Form()],
@@ -63,5 +64,19 @@ async def get_image(item_id):
                               SELECT image FROM items WHERE id={item_id}
                               """).fetchone()[0]
     return Response(content=bytes.fromhex(image_bytes))
+
+@app.post('/signup')
+def signup(id:Annotated[str,Form()],
+           password:Annotated[str,Form()],
+           name:Annotated[str,Form()],
+           email:Annotated[str,Form()]
+           ):
+    cur.execute(f"""
+                INSERT INTO users(id,name,email,password)
+                VALUES ('{id}','{name}','{email}','{password}')
+                """)
+    con.commit()
+    return "200"
+    
 
 app.mount("/", StaticFiles(directory="frontend",html="True"), name="frontend")
